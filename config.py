678c1badc39a1673d03664069d9fbd3280a049ee
CRYSTAL HOSTING <crystal.hq.hosting@gmail.com>
2023-05-30 19:32:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,15 +29,15 @@ PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
+DB_NAME = os.environ.get("DATABASE_NAME", "")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", ""))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
+START_MSG = os.environ.get("START_MESSAGE", "Hello {mention} 👋\n\nI Am Shigeo Kageyama Aka Mob !\n\nJoin Our Channel To Get Access To Files Through Link !")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
@@ -46,13 +46,13 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "🗣️ Kageyama:\nHello {mention} 👋\nYou Need To Join Our Channel, So You Can Access My Links !\n\nThen Tap On ♻️<b>TRY AGAIN</b> To Receive Files !")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
 #set True if you want to prevent users from forwarding files from bot
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
 
 #Set true if you want Disable your Channel Posts Share button
 if os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True':
@@ -60,13 +60,13 @@ if os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True':
 else:
     DISABLE_CHANNEL_BUTTON = False
 
-BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+BOT_STATS_TEXT = "🤖<b>BOT UPTIME</b>📊\n{uptime}"
+USER_REPLY_TEXT = "🔔 Telegram Notification:\n\nKageyama Is Busy With Body Improvement Club Activities, So He Can't Talk To You !\nYou Can Type /start For Help !\n\nOk BaiBai 👋\n\n👀: bit.ly/3oE1k0W"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
 
-LOG_FILE_NAME = "filesharingbot.txt"
+LOG_FILE_NAME = "Kageyamadiary.txt"
 
 logging.basicConfig(
     level=logging.INFO,
